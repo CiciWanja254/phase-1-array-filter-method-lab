c886ae36d385ab1ab4664608c35f2da4494add50
Cici254 <thukucindy00@gmail.com>
2023-03-19 23:46:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+  }
+  
